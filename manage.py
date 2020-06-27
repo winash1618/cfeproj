d207b9c841e5791eb71025070eb98876a4bb407f
winash786 <46051448+winash786@users.noreply.github.com>
2020-06-27 14:26:32 +0530

    gh

--- manage.py
+++ manage.py
@@ -14,8 +14,6 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-    if '--nostatic' not in sys.argv:
-        sys.argv.append('--nostatic')
     execute_from_command_line(sys.argv)
     
 
