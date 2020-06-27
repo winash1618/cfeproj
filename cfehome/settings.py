44e85fc9f8fb6b32a591b1db9fe151646f4f4b86
winash786 <46051448+winash786@users.noreply.github.com>
2020-06-27 08:52:50 +0530

    e

--- cfehome/settings.py
+++ cfehome/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    #'django.contrib.staticfiles',
+    'django.contrib.staticfiles',
     'pages.apps.PagesConfig',
 ]
 
@@ -118,4 +118,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/--nostatic/'
+STATIC_URL = '/static/'
